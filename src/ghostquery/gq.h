384fd58461adc3cbbaa9c1650b735acbf1cf46cb
redline1736 <theman1827@proton.me>
2026-09-05 23:25:34 -0400

    final fix

--- src/ghostquery/gq.h
+++ src/ghostquery/gq.h
@@ -3,7 +3,7 @@
 #include "../global.h"
 int sql_run(void);
 int xss_run(char *url, char *path);
-int find_param_reflecting(char *url);
+int find_param_reflecting(char *url, char *path);
 void xss_reflect(char *url, char *path);
 void xss_custom(char *url, char *path);  /* NEW: handles mouse/click/link XSS events */
 
